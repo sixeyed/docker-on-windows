2db0a08e691e7bb5b2b2696011c7311214c2ecc7
Elton Stoneman <elton@sixeyed.com>
2018-11-22 11:02:41 +0000

    Updates for Windows 2019 LTSC release

---
+++ ch01/build.ps1
@@ -0,0 +1,3 @@
+docker image build -t dockeronwindows/ch01-az:2e ./ch01-az
+
+docker image build -t dockeronwindows/ch01-whale:2e ./ch01-whale
---
+++ ch03/build.ps1
@@ -0,0 +1,6 @@
+docker image build -t dockeronwindows/ch03-iis-environment-variables:2e ./ch03-iis-environment-variables
+
+docker image build -t dockeronwindows/ch03-iis-environment-variables:2e -f Dockerfile.servicemonitor ./ch03-iis-environment-variables
+
+docker image build -t dockeronwindows/ch03-iis-healthcheck:2e ./ch03-iis-healthcheck
+
