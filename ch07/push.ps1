962ab929124db501ed9e6cca3831d94c49146edd
Elton Stoneman <elton@sixeyed.com>
2019-02-15 14:11:24 +0000

    Add app update & Azure config

--- ch07/push.ps1
+++ ch07/push.ps1
@@ -1,8 +1,7 @@
 docker image push dockeronwindows/ch07-nerd-dinner-web:2e
 
-docker image push dockeronwindows/ch07-nerd-dinner-index-handler:2e
-
 docker image push dockeronwindows/ch07-nerd-dinner-api:2e
 
 docker image push dockeronwindows/ch07-nerd-dinner-db:2e 
 
+docker image push dockeronwindows/ch07-nerd-dinner-homepage:2e
